f56c0d962560912c51196421f97d1373972d5e99
DAMN1SELF <damn1self@gmail.com>
2025-08-10 15:16:14 -0500

    Proyecto Inicial

---
+++ servicio_alumno/src/main/java/damn1self/com/servicio_alumno/ServicioAlumnoApplication.java
@@ -0,0 +1,13 @@
+package damn1self.com.servicio_alumno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAlumnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioAlumnoApplication.class, args);
+    }
+
+}
---
+++ servicio_alumno/src/test/java/damn1self/com/servicio_alumno/ServicioAlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package damn1self.com.servicio_alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAlumnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
